49593377d68d10df8c8c448e46ad0db6d540bba9
Antonio Iorio <antonioiorio94@gmail.com>
2022-03-03 16:03:10 +0100

    aggiunta richiesta cognome + richiesta colore

--- script/index.js
+++ script/index.js
@@ -2,3 +2,16 @@ const nomeUtente = prompt("Inserisci il tuo nome");
 
 document.getElementById('nome').innerText = nomeUtente;
 
+
+const cognomeUtente = prompt("Inserisci il tuo cognome");
+
+document.getElementById("cognome").innerText = cognomeUtente;
+
+
+const colorePreferito = prompt("Dimmi il tuo colore preferito");
+
+document.getElementById("colore").innerText = colorePreferito;
+
+const nuovaPassword = (nomeUtente + cognomeUtente + colorePreferito + "21");
+
+console.log(nuovaPassword);
